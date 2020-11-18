8022f80cbbca55445bc2f0d31abc139f97a33ab0
Amelia Roberts <arob0017@student.monash.edu>
2020-11-18 18:08:11 +1100

    adding prev search

--- search.js
+++ search.js
@@ -2,48 +2,57 @@ var recentSearch = [];
 var recipe;
 var storedRecipes = JSON.parse(window.localStorage.getItem("storedRecipes")) || [];
 
-// function recentlySearched() {
+function recentlySearched() {
 
-// };
+    $("#searchResults").empty();
+
+    if (storedRecipes) {
+        $("#recentlySearched").empty();
+
+        var btns = $("<div>").attr("class", "buttons");
+        for (var i = 0; storedRecipes.length > i; i++) {
+            var recipeBtn = $("<button>").attr("href", "#").attr("id", "recipeBtn").text(recentSearch[i]);
+            recipeBtn.attr("class", "uk-button uk-button-primary uk-button-large");
+            btns.prepend(recipeBtn);
+            $("#recentlySearched").append(btns);
+        }
+    }
+};
 
 function searchRecipes(recipe) {
-    // recentlySearched();
+    recentlySearched();
 
     $.ajax({
-        url: "https://api.edamam.com/search?q=" + recipe + "&app_id=b208d965&app_key=168bd55cfd40d63ac22f125ce7280e08&from=0&to=10",
+        url: "https://api.edamam.com/search?q=" + recipe + "&app_id=b208d965&app_key=168bd55cfd40d63ac22f125ce7280e08&from=0&to=4",
         method: "GET",
         dataType: "json",
         success: function (data) {
             console.log(data);
         }
     }).then(function (data) {
-        console.log(data);
+
+        $("#searchResults").empty();
+        //loop through array response to find the forecasts
+        for (var i = 1; i <= 10; i++) {
+
+            var newCard = $("<div>").attr("class", "uk-child-width-1-2@s uk-grid-match");
+
+            var bodyDiv = $("<div>").attr("class", "uk-card uk-card-hover uk-card-body");
+            var footerDiv = $("<div>").attr("class", "uk-card-footer")
+
+            var cardTitle = $("<div>").attr("class", "uk-card-title").text(data.hits[i].recipe.label);
+            bodyDiv.append(cardTitle);
+
+            bodyDiv.append($("<p>").attr("class", "card-text").html(data.hits[i].recipe.healthLabels));
+            bodyDiv.append($("<p>").attr("class", "card-text").text("Calories: " + data.hits[i].recipe.totalTime));
+            bodyDiv.append($("<p>").attr("class", "card-text").text("Total Time: " + data.hits[i].recipe.calories));
+
+            footerDiv.append($("<a>").attr("href", (data.hits[i].recipe.url)).attr("class", "uk-button uk-button-text").text("View Full Recipe Here"));
+            newCard.append(bodyDiv);
+            bodyDiv.append(footerDiv)
+            $("#searchResults").append(newCard);
+        };
     })
-
-    $("#searchResults").empty();
-    //loop through array response to find the forecasts
-    for (var i = 1; i <= 10; i++) {
-
-        var newCard = $("<div>").attr("class", "uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin");
-
-        var cardImg = $("<img>").attr("class", "uk-flex-last@s uk-card-media-right uk-cover-container").attr("src", "https://www.edamam.com/web-img/" + recipe.hits[i].recipe.image + "@2x.jpg");
-        newCard.append(cardImg);
-
-        var bodyDiv = $("<div>").attr("class", "uk-card-body");
-
-
-        var cardTitle = $("<div>").attr("class", "uk-card-title").text(recipe.hits[i].recipe.label);
-        newCard.append(cardTitle);
-
-
-        bodyDiv.append($("<p>").attr("class", "card-text").html(recipe.hits[i].recipe.healthLabels));
-        bodyDiv.append($("<p>").attr("class", "card-text").text("Calories: " + recipe.hits[i].recipe.totalTime));
-        bodyDiv.append($("<p>").attr("class", "card-text").text("Total Time: " + recipe.hits[i].recipe.calories));
-
-        newCard.append(bodyDiv);
-        $("#searchResults").append(newCard);
-
-    };
 }
 // Event handler for user clicking the search forecast button
 $("#searchBtn").on("click", function (event) {
@@ -54,7 +63,14 @@ $("#searchBtn").on("click", function (event) {
     if (recipe != "") {
         storedRecipes.push(recipe);
         localStorage.setItem("storedRecipes", JSON.stringify(storedRecipes));
-        // recentlySearched():
+        recentlySearched();
         searchRecipes(recipe);
     }
+});
+
+$("#recentlySearched").on("click", "button", function (event) {
+    event.preventDefault();
+    var prevRecipe = $(this).text();
+
+    searchRecipes(prevRecipe, recipe);
 });
